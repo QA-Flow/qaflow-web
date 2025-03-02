2bd16df15191344c9d84b1f7ff8de446f511cb51
Doruk Özgen <dorukozgen0@gmail.com>
2025-03-02 19:20:29 +0100

    refactor: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
 
 const nextConfig: NextConfig = {
-  output: "standalone"
+  output: "standalone",
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
